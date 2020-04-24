f7a0685a11b67df5534a61a85423b652d0107887
Bania Fonseca <baniafonseca@outlook.com>
2020-04-24 22:49:57 +0200

    first launch

---
+++ deeplibrary/deeplibrary/urls.py
@@ -0,0 +1,22 @@
+"""deeplibrary URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+"""
+# Django imports
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from . import views 
+
+urlpatterns = [
+    url(r'^$', views.home),
+    # provide the most basic login/logout functionality
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='core/login.html'),
+        name='core_login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='core_logout'),
+
+    # enable the admin interface
+    url(r'^admin/', admin.site.urls),
+]
