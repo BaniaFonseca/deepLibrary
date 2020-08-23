443a68fec29a2aa67950e19d19386a49281b9002
BaniaFonseca <baniafonseca@outlook.com>
2020-08-23 21:19:59 +0200

    deployment is failing

--- dl/settings/common.py
+++ dl/settings/common.py
@@ -116,18 +116,18 @@ MEDIA_URL = '/media/'
 # ##### DEBUG CONFIGURATION ###############################
 DEBUG = False
 
-# finally grab the SECRET KEY
-try:
-    SECRET_KEY = open(SECRET_FILE).read().strip()
-except IOError:
-    try:
-        from django.utils.crypto import get_random_string
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!$%&()=+-_'
-        SECRET_KEY = get_random_string(50, chars)
-        with open(SECRET_FILE, 'w') as f:
-            f.write(SECRET_KEY)
-    except IOError:
-        raise Exception('Could not open %s for writing!' % SECRET_FILE)
+# # finally grab the SECRET KEY
+# try:
+#     SECRET_KEY = open(SECRET_FILE).read().strip()
+# except IOError:
+#     try:
+#         from django.utils.crypto import get_random_string
+#         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!$%&()=+-_'
+#         SECRET_KEY = get_random_string(50, chars)
+#         with open(SECRET_FILE, 'w') as f:
+#             f.write(SECRET_KEY)
+#     except IOError:
+#         raise Exception('Could not open %s for writing!' % SECRET_FILE)
 
 # # Activate Django-Heroku.
 django_heroku.settings(locals())
